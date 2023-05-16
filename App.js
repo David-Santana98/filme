d53026966a0e667fdf00d6788d462073560e39a6
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-05-16 11:16:25 -0300

    criando a view

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet,Text, Image, View } from 'react-native';
 import SessionTitle from './components/SessionTitle';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <SessionTitle>Filmes em Alta</SessionTitle>
+
+      <View>
+        <Image></Image>
+        <View>
+          <Text>⭐</Text>
+          <Text>8.9</Text>
+        </View>
+      </View>
 
       <SessionTitle>Melhores Dramas</SessionTitle>
       <StatusBar style="auto" />
