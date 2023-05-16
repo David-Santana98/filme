5f80a0f1222dfd7eeddb4892f952b258461f09e7
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-05-15 21:53:44 -0300

    criando um arquivo React Native

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Filmes em Alta</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#212121',
     justifyContent: 'center',
   },
+  title: {
+    color: '#FFF',
+    fontSize: 16,
+    borderLeftColor: '#f06',
+    borderLeftWidth: 5,
+    padding: 5,
+    lineHeight: 16,
+     }
 });
